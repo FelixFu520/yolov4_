7d095356084f8456ae7fec6106ae1547d7a952d5
fusimeng <fufaloveguomengkai@163.com>
2020-11-05 17:52:10 +0800

    生成anchors

--- kmeans_for_anchors.py
+++ kmeans_for_anchors.py
@@ -3,58 +3,72 @@ import xml.etree.ElementTree as ET
 import glob
 import random
 
-def cas_iou(box,cluster):
-    x = np.minimum(cluster[:,0],box[0])
-    y = np.minimum(cluster[:,1],box[1])
+
+def cas_iou(box, cluster):
+    """
+    计算一个anchor到9个中心簇的距离
+    :param box: size （2，） eg. [0.218, 0.32]
+    :param cluster:  size （9，2） eg. [[0.088, 0.122], [0.99, 0.734], ...]
+    :return:
+    """
+    x = np.minimum(cluster[:, 0], box[0])
+    y = np.minimum(cluster[:, 1], box[1])
 
     intersection = x * y
     area1 = box[0] * box[1]
 
-    area2 = cluster[:,0] * cluster[:,1]
+    area2 = cluster[:, 0] * cluster[:, 1]
     iou = intersection / (area1 + area2 -intersection)
 
     return iou
 
-def avg_iou(box,cluster):
-    return np.mean([np.max(cas_iou(box[i],cluster)) for i in range(box.shape[0])])
+
+def avg_iou(box, cluster):
+    return np.mean([np.max(cas_iou(box[i], cluster)) for i in range(box.shape[0])])
 
 
-def kmeans(box,k):
+def kmeans(box, k):
     # 取出一共有多少框
-    row = box.shape[0]
+    row = box.shape[0]  # 30638
     
     # 每个框各个点的位置
-    distance = np.empty((row,k))
+    distance = np.empty((row, k))   # size (30638, 9)
     
     # 最后的聚类位置
-    last_clu = np.zeros((row,))
+    last_clu = np.zeros((row,))     # size (30638)
 
     np.random.seed()
 
-    # 随机选5个当聚类中心
-    cluster = box[np.random.choice(row,k,replace = False)]
+    # 随机选9个当聚类中心
+    cluster = box[np.random.choice(row, k, replace=False)]  # size (9,2) eg. [[0.088, 0.12],...]
     # cluster = random.sample(row, k)
     while True:
         # 计算每一行距离五个点的iou情况。
         for i in range(row):
-            distance[i] = 1 - cas_iou(box[i],cluster)
+            distance[i] = 1 - cas_iou(box[i], cluster)
         
         # 取出最小点
-        near = np.argmin(distance,axis=1)
+        near = np.argmin(distance, axis=1)  # size 30638, 30638个框到9个簇心的最短距离的位置
 
         if (last_clu == near).all():
             break
         
         # 求每一个类的中位点
-        for j in range(k):
+        for j in range(k):  # 第j个类
             cluster[j] = np.median(
-                box[near == j],axis=0)
+                box[near == j], axis=0)     # box[near==j]表示 30638个框到第0类最近的所有框   的中位数
 
         last_clu = near
 
     return cluster
 
+
 def load_data(path):
+    """
+    VOC 数据集每个图片的标注*.xml
+    :param path:
+    :return: 返回所有图像中每个object的w和h
+    """
     data = []
     # 对于每一个xml都寻找box
     for xml_file in glob.glob('{}/*xml'.format(path)):
@@ -73,7 +87,7 @@ def load_data(path):
             xmax = np.float64(xmax)
             ymax = np.float64(ymax)
             # 得到宽高
-            data.append([xmax-xmin,ymax-ymin])
+            data.append([xmax-xmin, ymax-ymin])
     return np.array(data)
 
 
@@ -87,12 +101,12 @@ if __name__ == '__main__':
     
     # 载入所有的xml
     # 存储格式为转化为比例后的width,height
-    data = load_data(path)
+    data = load_data(path)  # size 30638 eg. [[0.21, 0.32], [0.10, 0.32], ...]
     
     # 使用k聚类算法
-    out = kmeans(data,anchors_num)
-    out = out[np.argsort(out[:,0])]
-    print('acc:{:.2f}%'.format(avg_iou(data,out) * 100))
+    out = kmeans(data, anchors_num)
+    out = out[np.argsort(out[:, 0])]
+    print('acc:{:.2f}%'.format(avg_iou(data, out) * 100))
     print(out*SIZE)
     data = out*SIZE
     f = open("yolo_anchors.txt", 'w')
