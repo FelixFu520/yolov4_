b614899e801c9356b9e64cddf3b1e51882f5a0e0
fusimeng <fufaloveguomengkai@163.com>
2020-12-29 15:32:03 +0800

    deploy pytorch

---
+++ deploy/pytorch_pc_deploy/predict.py
@@ -0,0 +1,22 @@
+#-------------------------------------#
+#       对单张图片进行预测
+#-------------------------------------#
+from yolo import YOLO
+from PIL import Image
+
+yolo = YOLO()
+
+while True:
+    # img = input('Input image filename:')
+    try:
+        # image = Image.open(img)
+        image = Image.open("street.jpg")
+    except:
+        print('Open Error! Try again!')
+        continue
+    else:
+        r_image = yolo.detect_image(image)
+        r_image.show()
+        r_image.save("street_p.jpg")
+
+    break
